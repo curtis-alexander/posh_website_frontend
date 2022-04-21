6f9089c49a922a1c306812a47e99f8f1f147a084
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2022-04-21 15:44:08 -0500

    add nav bar links to corresponding pages

--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,12 @@
           <div class="collapse navbar-collapse" id="navbarResponsive">
               <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
               <li class="nav-ite m">
-                <li class="nav-item"><a class="nav-link" href="#services">Services</a>
             <li class="nav-item">
-                    <li class="nav-item"><a class="nav-link" href="#gallery">Gallery</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-                </ul>
+                <li class="nav-item"><a class="nav-link" href="/">Home</a>
+                <li class="nav-item"><a class="nav-link" href="/services">Services</a>
+                <li class="nav-item"><a class="nav-link" href="/gallery">Gallery</a></li>
+                <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
+              </ul>
           </div>
         </div>
       </nav>
