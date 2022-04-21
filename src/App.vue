306f54aa46db30bddb023ed78409d6e6f055e3ef
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2022-04-20 22:50:40 -0500

    add bootstrap header/footer to app.vue and change bg pic to salon

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,49 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/services">Services</router-link> |
-      <router-link to="/gallery">Gallery</router-link>
+    <div id="header">
+    <!-- Navigation-->
+      <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+        <div class="container">
+          <a class="navbar-brand" href="#header"
+            ><img src="assets/img/posh-logo.jpeg" alt="@#$%"
+          /></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars ms-1"></i>
+            </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+              <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+              <li class="nav-ite m">
+                <li class="nav-item"><a class="nav-link" href="#services">Services</a>
+            <li class="nav-item">
+                    <li class="nav-item"><a class="nav-link" href="#gallery">Gallery</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                </ul>
+          </div>
+        </div>
+      </nav>
     </div>
+
     <router-view />
+  
+    <footer class="footer py-4">
+      <div class="container">
+          <div class="row align-items-center">
+              <div class="col-lg-4 text-lg-start">Copyright &copy; MaxMeik</div>
+              <div class="col-lg-4 my-3 my-lg-0">
+                  <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
+                  <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
+                  <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
+              </div>
+              <div class="col-lg-4 text-lg-end">
+                  <a class="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a>
+                  <a class="link-dark text-decoration-none" href="#!">Terms of Use</a>
+              </div>
+          </div>
+      </div>
+    </footer> 
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <!-- <h1>{{ message }}</h1> -->
+    <header class="masthead">
+      <div class="container">
+        <div class="masthead-subheading">Welcome To Our Salon!</div>
+        <div class="masthead-heading text-uppercase">Posh Hair Boutique</div>
+        <a class="btn btn-primary btn-xl text-uppercase" href="#services"
+          >Tell Me More</a
+        >
+      </div>
+  </header>
   </div>
 </template>
 
