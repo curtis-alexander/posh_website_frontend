905630772852680b0ca0a611143c97a3a32c6805
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2022-04-22 13:03:44 -0500

    complete about page text/pics

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,7 +4,7 @@
       <div class="container">
           <div class="text-center">
               <h2 class="section-heading text-uppercase">About</h2>
-              <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+              <h3 class="section-subheading text-muted">Posh Hair Boutique</h3>
           </div>
           <ul class="timeline">
               <li>
@@ -14,48 +14,67 @@
                           <!-- <h4>2009-2011</h4> -->
                           <h4 class="subheading">Meet the Owner</h4>
                       </div>
-                      <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
+                      <div class="timeline-body"><p class="text-muted">I have been a hairstylist since 2005. I opened Posh in 2008.</p></div>
                   </div>
               </li>
               <li class="timeline-inverted">
-                  <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/2.jpg" alt="..." /></div>
+                  <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/Calura-img.jpg" alt="..." /></div>
                   <div class="timeline-panel">
                       <div class="timeline-heading">
-                          <h4>March 2011</h4>
-                          <h4 class="subheading">An Agency is Born</h4>
+                          <h4 class="subheading">CALURA Permanent Colour</h4>
                       </div>
-                      <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
+                      <div class="timeline-body"><p class="text-muted">The CALURA colour system is ammonia free, PPD free & gluten free. It is formulated with an innovative exothermic technology that was designed to minimize hair damage. As a bonus, this heat allows a thorough penetration of the certified organic essences and keratin.
+                      Organic AVOCADO Oil: Enhancing hydration & porosity equalizer.
+                      Organic POMEGRANATE Seed Oil: Contributes to the longevity and shine of the colour.
+                      Organic Evening PRIMROSE Oil: Keeping moisture inside the cortex of the hair.
+                      Organic SHEA Butter: Protects hair from heat & chemicals.
+                      The result is an incomparable radiant shine!</p></div>
                   </div>
               </li>
               <li>
                   <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/3.jpg" alt="..." /></div>
                   <div class="timeline-panel">
                       <div class="timeline-heading">
-                          <h4>December 2015</h4>
-                          <h4 class="subheading">Transition to Full Service</h4>
+                          <h4 class="subheading">Blacklight Lightening System</h4>
                       </div>
-                      <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
+                      <div class="timeline-body"><p class="text-muted">Blacklight’s technology offers vibrant blondes, while maintaining shiny, healthy and hydrated hair. Happy Hair. Happy Life.
+                    </p></div>
                   </div>
               </li>
               <li class="timeline-inverted">
                   <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/4.jpg" alt="..." /></div>
                   <div class="timeline-panel">
                       <div class="timeline-heading">
-                          <h4>July 2020</h4>
-                          <h4 class="subheading">Phase Two Expansion</h4>
+                          <h4 class="subheading">Quadraplex</h4>
                       </div>
-                      <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
+                      <div class="timeline-body"><p class="text-muted">Quadraplex is the most complete system of its category. It was created to offer the following 4 essential benefits to your hair: it strengthens, protects, detoxifies and repairs. Add Quadraplex to your colour and lightening services to prevent hair damage and breakage & also ensure healthier, stronger, shinier and more beautiful hair.</p></div>
+                  </div>
+              </li>
+              <li>
+                  <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/3.jpg" alt="..." /></div>
+                  <div class="timeline-panel">
+                      <div class="timeline-heading">
+                          <h4 class="subheading">Oligo’s Care Products</h4>
+                      </div>
+                      <div class="timeline-body"><p class="text-muted">Oligo’s care products are 100% vegan as well as completely sulfate, salt and paraben free! Only the best ingredients have been selected to ensure optimal hydration, shine and colour retention.
+
+                    </p></div>
                   </div>
               </li>
               <li class="timeline-inverted">
-                  <div class="timeline-image">
-                      <h4>
-                          Be Part
-                          <br />
-                          Of Our
-                          <br />
-                          Story!
-                      </h4>
+                  <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/4.jpg" alt="..." /></div>
+                  <div class="timeline-panel">
+                      <div class="timeline-heading">
+                          <h4 class="subheading">Cezzane Perfect Finish</h4>
+                      </div>
+                      <div class="timeline-body"><p class="text-muted">Text here Text here Text here Text here Text here Text here Text here Text here Text here Text here </p></div>
+                  </div>
+              </li>
+              <li class="timeline-inverted">
+                  <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/best-seller.png" alt="..." /></div>
+                  <div class="timeline-panel">
+                      <div class="timeline-heading">
+                      </div>
                   </div>
               </li>
           </ul>
