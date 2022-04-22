0174e7150844e88e605cbd6df124f0ca57b1da57
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2022-04-22 12:27:05 -0500

    add fb and ig links in footer and change color of navbar hover tabs

--- src/App.vue
+++ src/App.vue
@@ -31,9 +31,9 @@
           <div class="row align-items-center">
               <div class="col-lg-4 text-lg-start">Copyright &copy; MaxMeik</div>
               <div class="col-lg-4 my-3 my-lg-0">
-                  <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
-                  <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
-                  <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
+                  <a class="btn btn-dark btn-social mx-2" href="https://instagram.com/corisposhhairboutique/" aria-label="Instagram"><i class="fab fa-instagram"></i></a>
+                  <a class="btn btn-dark btn-social mx-2" href="https://facebook.com/corisposhhairboutique" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
+                  <!-- <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="LinkedIn"><i class="fab fa-linkedin-in"></i></a> -->
               </div>
               <div class="col-lg-4 text-lg-end">
                   <a class="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a>
