25937070281f75fb33337f0fc76da5621d133f8f
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2022-04-25 09:24:08 -0500

    add the rest of pics and descriptions on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,13 +8,13 @@
           </div>
           <ul class="timeline">
               <li>
-                  <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/1.jpg" alt="..." /></div>
+                  <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/meet-the-owner.jpg" alt="..." /></div>
                   <div class="timeline-panel">
                       <div class="timeline-heading">
                           <!-- <h4>2009-2011</h4> -->
                           <h4 class="subheading">Meet the Owner</h4>
                       </div>
-                      <div class="timeline-body"><p class="text-muted">I have been a hairstylist since 2005. I opened Posh in 2008.</p></div>
+                      <div class="timeline-body"><p class="text-muted">I went to Empire Beauty School in Hanover Park IL. While in school, I worked as an assistant at Heidi’s Salon in Woodfield Mall where I learned a lot about coloring, toning & haircutting. I have been a hairstylist since 2005. I worked at an Aveda concept salon for 3 years before I opened Posh in 2008. I have taken numerous coloring, foiling, cutting & styling training courses. I am an expert colorist and specialize in foiling, foilayage & balayage. I stay up to date on trends & techniques. I offer a variety of services including coloring, foiling, haircuts, styling, facial waxing & hair extensions. I use high quality & non toxic products in my chemical free salon. </p></div>
                   </div>
               </li>
               <li class="timeline-inverted">
@@ -32,17 +32,17 @@
                   </div>
               </li>
               <li>
-                  <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/3.jpg" alt="..." /></div>
+                  <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/blacklight-system.jpg" alt="..." /></div>
                   <div class="timeline-panel">
                       <div class="timeline-heading">
                           <h4 class="subheading">Blacklight Lightening System</h4>
                       </div>
-                      <div class="timeline-body"><p class="text-muted">Blacklight’s technology offers vibrant blondes, while maintaining shiny, healthy and hydrated hair. Happy Hair. Happy Life.
+                      <div class="timeline-body"><p class="text-muted">Blacklight’s technology offers vibrant blondes, while maintaining shiny, healthy and hydrated hair.</p><p class="text-muted"> Happy Hair. Happy Life.
                     </p></div>
                   </div>
               </li>
               <li class="timeline-inverted">
-                  <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/4.jpg" alt="..." /></div>
+                  <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/Quadraplex.jpg" alt="..." /></div>
                   <div class="timeline-panel">
                       <div class="timeline-heading">
                           <h4 class="subheading">Quadraplex</h4>
@@ -51,7 +51,7 @@
                   </div>
               </li>
               <li>
-                  <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/3.jpg" alt="..." /></div>
+                  <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/Oligo-products.jpg" alt="..." /></div>
                   <div class="timeline-panel">
                       <div class="timeline-heading">
                           <h4 class="subheading">Oligo’s Care Products</h4>
@@ -62,12 +62,43 @@
                   </div>
               </li>
               <li class="timeline-inverted">
-                  <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/4.jpg" alt="..." /></div>
+                  <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/blacklight-purple.jpg" alt="..." /></div>
                   <div class="timeline-panel">
                       <div class="timeline-heading">
-                          <h4 class="subheading">Cezzane Perfect Finish</h4>
+                          <h4 class="subheading">Blacklight’s Care Products</h4>
                       </div>
-                      <div class="timeline-body"><p class="text-muted">Text here Text here Text here Text here Text here Text here Text here Text here Text here Text here </p></div>
+                      <div class="timeline-body"><p class="text-muted">Blacklight’s care products are 100% vegan as well as completely sulfate, salt and paraben free! Ingredients make the difference. 11 amino acids moisturize hair, enhance hair strength, repair surface damage & improve hair shine. Argan oil naturally hydrates hair, enhances hair elasticity, restores hair smoothness & protects against UV rays. Blacklight’s blue & violet toned products correct gold/orange tones giving you the brightest blonde. </p></div>
+                  </div>
+              </li>
+              <li>
+                  <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/Olaplex1.jpg" alt="..." /></div>
+                  <div class="timeline-panel">
+                      <div class="timeline-heading">
+                          <h4 class="subheading">OLAPLEX</h4>
+                      </div>
+                      <div class="timeline-body"><p class="text-muted">OLAPLEX products are free of sulfates, phthalates, DEA, aldehydes, formaldehyde, gluten, parabens and safe for all hair! OLAPLEX assists in both preventing damage and repairing it, returning the hair to a virgin like state.
+                    </p></div>
+                  </div>
+              </li>
+              <li class="timeline-inverted">
+                  <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/Cezanne.jpg" alt="..." /></div>
+                  <div class="timeline-panel">
+                      <div class="timeline-heading">
+                          <h4 class="subheading">Cezanne Perfect Finish</h4>
+                      </div>
+              <div class="timeline-body">
+                <p class="text-muted">
+                  Cezanne Perfect Finish is a 100% formaldehyde/aldehyde and
+                  methylene glycol-FREE smoothing treatment. Cut down on
+                  styling time! Say goodbye to dry frizzy hair! Say hello to
+                  healthy hair! 👋  Cezanne Smoothing Treatment actually
+                  strengthens the hair. Using a revolutionary low pH technology
+                  that restores bonds that are damaged during thermal,
+                  mechanical or high pH services. It infuses a rich proprietary
+                  blend of keratin, aloe, botanical extracts, vitamins and shea
+                  butter esters for brilliant results that shine. The treatment
+                  lasts 3-5 months.
+                </p></div>
                   </div>
               </li>
               <li class="timeline-inverted">
