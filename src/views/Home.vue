460a3533c1b741cd01e8243d6e68d2c71e9329d4
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2022-05-07 10:47:56 -0500

    add about par to home page and change font size for address

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,19 +7,27 @@
         <div class="masthead-heading text-uppercase">Posh Hair Boutique</div> -->
       </div>
     </header>
-      <br>
-      <br>
+    <br>
+    <br>
     <div class="container">
       <div class="text-center">
         <div class="masthead-subheading">Cori Meiklejohn LLC</div>
         <h1>Posh Hair Boutique</h1>
-        <h6>for appointment call/text 608-931-6617</h6>
+        <h4>2640 Milton Ave, Suite 103</h4>
+        <br>
+        <div class="masthead-subheading">
+          <p>I love helping women feel beautiful & confident. I stay current on trends & cutting/coloring techniques/applications.</p>
+          <p>At Posh, I use/retail safe, natural & cruelty free hair products.</p>
+          <p>Enjoy your complimentary scalp massage in a fume FREE environment!</p>
+        </div>
+        <br>
+        <h6>Open by appointment only. To book, call/text 608-931-6617</h6>
+        <br>
         <h5 class="masthead-subheading">
           Great hair doesn't happen by chance, it happens by Appointment!
         </h5>
       </div>
     </div>
-
   </div>
 </template>
 
