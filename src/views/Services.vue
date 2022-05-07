026f496d773ecd4d39c4978d4985276364a05c30
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2022-05-07 12:17:33 -0500

    add all services list and pricing

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -4,42 +4,105 @@
     <div class="container">
       <div class="text-center">
         <h2 class="section-heading text-uppercase">Services</h2>
-        <h3 class="section-subheading text-muted">Here's what we offer</h3>
+        <h3 class="section-subheading text-muted">Here's what Posh offers</h3>
       </div>
-      <div class="row text-center">
-        <div class="col-md-4">
-          <span class="fa-stack fa-4x">
-            <i class="fas fa-circle fa-stack-2x text-primary"></i>
-            <i class="fas fa-sink fa-stack-1x fa-inverse"></i>
-          </span>
-          <h4 class="my-3">COLOR</h4>
-          <p class="text-muted">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-            maxime quam architecto quo inventore harum ex magni, dicta impedit.
-          </p>
+      <div class="row text-left">
+          <div class="col-md-4">
+            <!-- <span class="fa-stack fa-4x">
+              <i class="fas fa-circle fa-stack-2x text-primary"></i>
+              <i class="fas fa-sink fa-stack-1x fa-inverse"></i>
+            </span> -->
+          <h5 class="my-3 row text-center">HAIR COLOR</h5>
+        <p class="text-muted">All Over Color Root Retouch</p>
+        <p class="text-muted">All Over Color</p>
+        <p class="text-muted">Platinum Lightening Retouch</p>
+        <p class="text-muted">Partial Highlight</p>
+        <p class="text-muted">Full Head Highlight</p>
+        <p class="text-muted">Full Head Back to Back Highlight</p>
+        <p class="text-muted">All Over Color Root Retouch & Partial Highlight</p>
+        <p class="text-muted">All Over Color Root Retouch & Full Highlight</p>
+        <p class="text-muted">All Over Color & Partial Highlight</p>
+        <p class="text-muted">All Over Color & Full Highlight</p>
+        <p class="text-muted">Balayage/Foilayage</p>
+        <p class="text-muted">Balayage/Foilayage & Color Retouch</p>
+        <p class="text-muted">Toner/Gloss added to a color</p>
         </div>
         <div class="col-md-4">
-          <span class="fa-stack fa-4x">
-            <i class="fas fa-circle fa-stack-2x text-primary"></i>
-            <i class="fas fa-scissors fa-stack-1x fa-inverse"></i>
-          </span>
-          <h4 class="my-3">CUT</h4>
-          <p class="text-muted">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-            maxime quam architecto quo inventore harum ex magni, dicta impedit.
-          </p>
+          <h5 class="my-3 row text-center">Price</h5>
+          <p class="text-muted">$73</p>
+          <p class="text-muted">$88</p>
+          <p class="text-muted">$90</p>
+          <p class="text-muted">$90+</p>
+          <p class="text-muted">$120+</p>
+          <p class="text-muted">$130+</p>
+          <p class="text-muted">$120+</p>
+          <p class="text-muted">$135+</p>
+          <p class="text-muted">$125+</p>
+          <p class="text-muted">$140+</p>
+          <p class="text-muted">$150+</p>
+          <p class="text-muted">$170+</p>
+          <p class="text-muted">$15-20</p>
         </div>
         <div class="col-md-4">
-          <span class="fa-stack fa-4x">
-            <i class="fas fa-circle fa-stack-2x text-primary"></i>
-            <i class="fas fa-droplet fa-stack-1x fa-inverse"></i>
-            <!-- <i class="fa-solid fa-fondue-pot fa-stack-1x fa-inverse"></i> -->
-          </span>
-          <h4 class="my-3">WAX</h4>
-          <p class="text-muted">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-            maxime quam architecto quo inventore harum ex magni, dicta impedit.
-          </p>
+          <h5 class="my-3 row text-center">OLAPLEX & QUADRAPLEX</h5>
+          <h4 class="my-3 row text-center">Strengthen * Protect * Detoxify * Repair</h4>
+          <p class="text-muted">Added to your color service</p>
+          <p class="text-muted">New 3 in 1 Moisture Mask Treatment</p>
+        </div>
+        <div class="col-md-4">
+          <h5 class="my-3 row text-center">Price</h5>
+          <h4 class="my-3 row text-muted">.</h4>
+          <p class="text-muted">$15+</p>
+          <p class="text-muted">$10</p>
+        </div>
+        <div class="col-md-4">
+          <h5 class="my-3 row text-center">HAIR CUT & STYLING</h5>
+          <p class="text-muted">Women's Hair Cut</p>
+          <p class="text-muted">--Includes a shampoo, scalp massage, blow out & simple style</p>
+          <p class="text-muted">--Additional charge for curling</p>
+          <p class="text-muted">Thermal Styling/Curling</p>
+          <p class="text-muted">--Added to a haircut</p>
+          <p class="text-muted">Kid's Hair Cut (12 & under)</p>
+          <p class="text-muted">--short/med/long</p>
+          <p class="text-muted">Men's Hair Cut</p>
+          <p class="text-muted">Shampoo & Blow Out short/long</p>
+          <p class="text-muted">--Add thermal style</p>
+        </div>
+        <div class="col-md-4">
+          <h5 class="my-3 row text-center">Price</h5>
+          <p class="text-muted">$43</p>
+          <p class="text-muted">.</p>
+          <p class="text-muted">.</p>
+          <p class="text-muted">$5-10</p>
+          <p class="text-muted">.</p>
+          <p class="text-muted">$25/$30/$35</p>
+          <p class="text-muted">.</p>
+          <p class="text-muted">$30</p>
+          <p class="text-muted">$30/$35</p>
+          <p class="text-muted">$5-10</p>
+        </div>
+        <div class="col-md-4">
+          <h5 class="my-3 row text-center">CEZANNE PROTEIN & SMOOTHING TREATMENT</h5>
+          <p class="text-muted">Initial price includes the first 1.5oz. $50 per oz therafter.</p>
+          <p class="text-muted">Includes a complimentary dusting/trim of any split ends.</p>
+        </div>
+        <div class="col-md-4">
+          <h5 class="my-3 row text-center">Price</h5>
+          <p class="text-muted">$225+</p>
+        </div>
+        <div class="col-md-4">
+          <h5 class="my-3 row text-center">WAXING</h5>
+          <p class="text-muted">Eyebrows</p>
+          <p class="text-muted">Upper Lip</p>
+          <p class="text-muted">Chin</p>
+          <p class="text-muted">Side Burns</p>
+        </div>
+        <div class="col-md-4">
+          <h5 class="my-3 row text-center">Price</h5>
+          <p class="text-muted">$17</p>
+          <p class="text-muted">$12</p>
+          <p class="text-muted">$10</p>
+          <p class="text-muted">$10</p>
         </div>
       </div>
     </div>
